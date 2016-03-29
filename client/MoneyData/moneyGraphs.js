7e70dc76d6fd75f3251155872f64ee0ba0337b17
Qian Wang <qwang2012@gmail.com>
2016-03-28 20:36:35 -0400

    Added month to month graph to moneyGraphs

--- client/MoneyData/moneyGraphs.js
+++ client/MoneyData/moneyGraphs.js
@@ -5,6 +5,7 @@
 	function moneyGraphsController($scope, loginDataService, authToken, $location, moneyChartsService) {
 
 		var plot;
+		var plot2;
 		var updateLegendTimeout = null;
 		var latestPosition = null;
 		var legends = $("#crosshair .legendLabel");
@@ -19,15 +20,20 @@
 			$scope.maxNet = calcMaxNetCost(data.entries);
 
 			var events = makeDataPoints(mergeSort(calculateDailyCosts(data.entries)));
+			var eventsMonthly = makeMonthlyDataPoints(mergeSort(calculateMonthlyCosts(data.entries)));
+
+			console.log(events);
+			console.log(eventsMonthly);
 
 			var sin = [], cos = [];
 			for (var i = 0; i < 14; i += 0.1) {
 				sin.push([i, Math.sin(i)]);
 				cos.push([i, Math.cos(i)]);
 			}
+
 			plot = $.plot($("#crosshair"), [{
 				data: events,
-				label: "New York = 0.00 °C"
+				label: "Daily Net Exchange"
 			}], {
 				series: {
 					lines: {show: true , lineWidth: 1},
@@ -64,6 +70,43 @@
 	      		yaxis: {ticks:6, tickDecimals: 0},
 
 				colors: [$blue],
+			});
+
+			plot2 = $.plot($("#verticalBar"), [{
+				data: eventsMonthly,
+			}], {
+		            series: {
+		              shadowSize: 0,
+		              bars: {
+		                lineWidth: 1,
+		                show: true,
+		                barWidth: 1000*60*60*24*28,
+		                }
+		            },
+		            grid: {
+		                hoverable: true,
+		                clickable: false,
+		                borderWidth: 1,
+		                tickColor: $border_color,
+		                borderColor: $grid_color,
+		                backgroundColor: { colors: ["#ffffff", "#ffffff"] }
+		            },
+		            legend:{   
+		                show: true,
+		                position: 'ne',
+		                noColumns: 0,
+		            },
+		            tooltip: true,
+		            tooltipOpts: {
+		                content: '$ %y'
+		            },
+					xaxis: {
+						mode: "time",
+						ticks:12, 
+						tickDecimals: 0
+					},
+
+		            colors: [$blue],
 			});
 
 			$("#crosshair").bind("plothover", function (event, pos, item) {
@@ -106,13 +149,13 @@
 				if (entry.isCost) cost = cost - entry.cost;
 				else cost = cost + entry.cost;
 			});
-			return cost;
+			return money_round(cost);
 		}
 
 		function calcAvgNetCost(entries) {
 			var cost = calcNetCost(entries);
 			var numDays = calculateDailyCosts(entries).length;
-			return cost/numDays;
+			return money_round(cost/numDays);
 		}
 
 		function calcMaxNetCost(entries) {
@@ -123,7 +166,7 @@
 					maxCost = entry.cost;
 				}
 			});
-			return maxCost;
+			return money_round(maxCost);
 		}
 
 		function calculateDailyCosts(entries) {
@@ -147,11 +190,40 @@
 			return dict;
 		};
 
+		function calculateMonthlyCosts(entries) {
+			var dict = [];
+			var index = [];
+			var tempDate = "";
+			entries.forEach(function(entry) {
+				tempDate = entry.year+"-"+entry.month+"-"+1;
+				if(index.indexOf(tempDate) == -1) {
+					index.push(tempDate);
+					dict[index.indexOf(tempDate)] = {
+						cost : 0,
+						day : 1,
+						month : entry.month,
+						year : entry.year
+					}
+				};
+				if(entry.isCost) dict[index.indexOf(tempDate)].cost = dict[index.indexOf(tempDate)].cost + entry.cost;
+				else dict[index.indexOf(tempDate)].cost = dict[index.indexOf(tempDate)].cost - entry.cost;
+			});
+			return dict;
+		};
+
 		function makeDataPoints(entries) {
 			var arr = [];
 			entries.forEach(function(entry) {
 				arr.push([gd(entry.year, entry.month, entry.day), entry.cost])
-			});	
+			});
+			return arr;
+		};
+
+		function makeMonthlyDataPoints(entries) {
+			var arr = [];
+			entries.forEach(function(entry) {
+				arr.push([gd(entry.year, entry.month, 1), entry.cost])
+			});
 			return arr;
 		};
 
@@ -198,5 +270,8 @@
 		    return result;
 		};
 
+		function money_round(num) {
+    		return Math.ceil(num * 100) / 100;
+		}
 	};
 }());
